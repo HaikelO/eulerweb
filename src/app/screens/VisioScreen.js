d476c3af45ad36c17d610994de1be67ecadea38b
HaikelO <haikel.ben@gmail.com>
2018-09-20 23:08:05 +0200

    socket io

--- src/app/screens/VisioScreen.js
+++ src/app/screens/VisioScreen.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import openSocket from 'socket.io-client';
+import ss from 'socket.io-stream';
+
 
 import './VisioScreen.css';
 
@@ -12,6 +15,8 @@ class VisioScreen extends Component {
     }
 
     componentDidMount(){
+        const socket = openSocket('http://localhost:8000/user');
+        const link = ss.createStream();
         this.getPermissions().then((stream)=>{
             this.setState({hasMedia: true});
             try {
@@ -19,7 +24,8 @@ class VisioScreen extends Component {
             } catch (e) {
                 this.publisher.src = URL.createObjectURL(stream);
             }
-
+            ss(socket).emit('stream', link);
+            ss.createBlobReadStream(URL.createObjectURL(stream)).pipe(link);
             this.publisher.play();
         });
     }
@@ -40,7 +46,7 @@ class VisioScreen extends Component {
             <div>
                 VisioScreen   
                 <video className='publisher' ref={(ref)=> {this.publisher = ref;}}></video>
-                {/* <video className='subscriber'></video>  */}            
+                <video className='subscriber' ref={(ref)=>{this.subscriber = ref;}}></video>           
             </div>
         );
     }
