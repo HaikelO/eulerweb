474299f63196bc9b6a90b7f1697fa4586b41fc28
HaikelO <haikel.ben@gmail.com>
2018-11-26 22:12:01 +0100

    Chat update

--- src/app/components/Chat/Chat.js
+++ src/app/components/Chat/Chat.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react'
 
 export class Chat extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      users: [],
+    };
+  }
+  renderMessage = (message) => {
+    return (
+      <div>
+        {message.value}
+      </div> 
+    );
+  };
+
+  renderMessages = (messages) => {
+    if(messages && messages.length > 0) {
+      return messages.map(this.renderMessage());
+    }
+     
+  }
+  
   render() {
     return (
       <div>
           <label>Chat</label>
           <div>
-            Say : Hello !
+            {this.renderMessages(this.state.messages)}
           </div>
           <textarea name="comment" form="usrform">Enter text here...</textarea>
-          
+          <button>Envoyer</button>
       </div>
     )
   }
